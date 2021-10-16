e19a3ad0e0195be2836776cda2f4cef0f8ae3184
liuyfab <liuyongfeng83@gmail.com>
2021-10-16 00:02:34 -0500

    update js code

--- assets/script.js
+++ assets/script.js
@@ -1,203 +1,195 @@
-var dataArr = []
-var body = document.body
-var questionIndex = 0
-var score = 0
-var timerEl = document.getElementById('time');
-var message = "Time's up!"
-var words = message.split('+');
-var timeLeft = 60;
-var timerId;
-var currentQ;
-var resultEl = document.getElementById("result")
-var questionEl = document.getElementById("questions")
-var scoreEl = document.getElementById("score");
-var submitScore = document.getElementById("btn")
-var scoreIdCounter =0
-var choicesEl = document.getElementById("choices");
-var start = document.getElementById('start');
-var startEl = document.getElementById('startScreen'); 
-var saveScore = document.getElementById('save-score')
-
-
-function countDown() {
-    timeLeft--
-    timerEl.textContent = timeLeft
-    if (timeLeft == 0) {
-        clearInterval(timerId)
-        timerEl.textContent = message;
-        // console.log(message);
-        endGame();
-        return timeLeft
-    }
-}
-
-function startQuiz() {
-    startEl.setAttribute("class", "hide")
-    // var questionEl = document.getElementById("questions")
-    questionEl.removeAttribute("class")
-    timerId = setInterval(countDown, 1000)
-    timerEl.textContent = timeLeft
-    getQuestion();
-}
-
-
-
-
 var questionList = [
     {
-        text: "What is the correct way to declare an array in JavaScript?",
-        choice1: 'var colors = "red", "green", "blue"',
-        choice2: 'var colors = ["red", "green", "blue"]',
-        choice3: 'var colors = (1:"red", 2:"green", 3:"blue")',
-        choice4: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")',
-        answer: "2"
+        title: "What operator is used to compare a value or variable?",
+        choices: ["=","===","#", "<<"],
+        answer: "==="
     },
     {
-        text: "What operator is used to compare a value or variable?",
-        choice1: "=",
-        choice2: "===",
-        choice3: "#",
-        choice4: "<<",
-        answer: "2"
+        title: "What operator is the AND operator?",
+        choices: ["+","&","&&","||"],
+        answer: "&&"
     },
     {
-        text: 'How do you write "Hello World" in an alert box?',
-        choice1: 'alert("Hello World");',
-        choice2: 'msgBox("Hello World");',
-        choice3: 'msg("Hello World");',
-        choice4: 'alertBox("Hello World");',
-        answer: "1"
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
     },
     {
-        text: "What operator is the AND operator?",
-        choice1: "+",
-        choice2: "&",
-        choice3: "&&",
-        choice4: "||",
-        answer: "3"
+        title: "What operator is the OR operator?",
+        choices: ["+","&","&&", "||"],
+        answer: "||"
     },
     {
-        text: "What type of event occurs when an HTML element is clicked on?",
-        choice1: "onchange",
-        choice2: "onmouseclick",
-        choice3: "onmouseover",
-        choice4: "onclick",
-        answer: "4"
+        title: 'How do you call a function named "myFunction"?',
+        choices: ["call function myFunction()","myFunction()","call myFunction()","call.myFunction()"],
+        answer: "myFunction()"
     },
     {
-        text: "What operator is the OR operator?",
-        choice1: "+",
-        choice2: "&",
-        choice3: "&&",
-        choice4: "||",
-        answer: "4"
-    },
-    {
-        text: 'How do you call a function named "myFunction"?',
-        choice1: "call function myFunction()",
-        choice2: "myFunction()",
-        choice3: "call myFunction()",
-        choice4: "call.myFunction()",
-        answer: "2"
-    },
-    {
-        text: "What operator is used to assign a value to a vairable?",
+        title: "What operator is used to assign a value to a vairable?",
         choices: [ "=", "===", "#", "<<"] ,
         answer: "="
     },
 ];
 
-function getQuestion() {
-    var currentQ = questionList[questionIndex]
-    console.log("Current Question " , currentQ);
-    questionEl.textContent = currentQ.text; 
+var score = 0;
+var questionIndex = 0;
+ 
+// select html elements.
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#StartTime");
+var questions = document.querySelector("#questions");
+var wrapper = document.querySelector("#wrapper");
 
-    for (var i = 0; i < currentQ.choices.length; i++) {
-        //Create a button 
-        var button = document.createElement("button")
-        button.setAttribute("id", [i]);
-        button.textContent = currentQ.choices[i]
+var secondsLeft = 60;
+var Interval = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
 
-        button.onclick = function (e) {
-            e.preventDefault(); 
-            console.log(e);
-            if (currentQ.answer == e.target.value) {
-                // var result = document.getElementById("result")
-                resultEl.textContent = "Correct" ;
-                // resultEl.appendChild(result)
-                buttonClick()
+timer.addEventListener("click", function () {
+    // We are checking zero because its originally set to zero
+    if (Interval === 0) {
+        Interval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(Interval);
+                allDone();
+                currentTime.textContent = "Time's up!";
             }
-            else {
-                resultEl.textContent = "Wrong";
-                timeLeft -= 10;
-            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+function render(questionIndex) {
+    // Clears existing data 
+    questions.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // For loops to loop through all info in array
+    for (var i = 0; i < questionList.length; i++) {
+        // Appends question title only
+        var userQuestion = questionList[questionIndex].title;
+        var userChoices = questionList[questionIndex].choices;
+        questions.textContent = userQuestion;
+    }
+    // New for each for question choices
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questions.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+// Event to compare choices with answer
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // Correct condition 
+        if (element.textContent == questionList[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questionList[questionIndex].answer;
+            // Correct condition 
+        } else {
+            // Will deduct -5 seconds off secondsLeft for wrong answers
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questionList[questionIndex].answer;
         }
-        // console.log(button)
-        choicesEl.appendChild(button)
-        // questions[i].question
+
     }
-   
-}
-
-// we want to check to see if button value is correct, increment question index, call get question function to retrieve next question, 
-// check for end condition
-function buttonClick() {
-    if (questionIndex < questions.length-1) {
-        questionIndex++
-        choicesEl.innerHTML = ""
-        getQuestion();
-
+    if (questionIndex >= questionList.length) {
+        // All done will append last page with user stats
+        allDone();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questionList.length + " Correct!";
     } else {
-        endGame();
-        //enterScore();
+        render(questionIndex);
     }
-}
-function endGame() {
-    clearInterval(timerId)
-    timerEl.textContent = timeLeft
-    score = timeLeft
-    // scoreEl.textContent += score;
-    scoreEl.removeAttribute("class")
-    console.log(scoreEl.textContent)
-    questionEl.setAttribute("class", "hide")
-    enterScore();
-}
+    questions.appendChild(createDiv);
 
-// not sure how to connect this to scores
-// need to connect it with high scores
+}   
 
-function enterScore(){
-    var scoreInitials = document.getElementById("initials").value.trim ();
-    var localStorageData = JSON.parse(localStorage.getItem("highScores"));
-    var scoreDataObj = {
-        initials: scoreInitials,
-        quizScore: timeLeft
+// All done will append last page
+function allDone() {
+    questions.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    // Heading:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+    questions.appendChild(createH1);
+
+    // Paragraph
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questions.appendChild(createP);
+
+    // Calculates time remaining and replaces it with score
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(Interval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questions.appendChild(createP2);
     }
-    scoreDataObj.id = scoreIdCounter;
 
-    dataArr.push(scoreDataObj);
-    localStorage.setItem("dataArr", JSON.stringify(dataArr))
-    scoreIdCounter++;
+    // Label
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questions.appendChild(createLabel);
+
+    // input
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questions.appendChild(createInput);
+
+    // submit
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questions.appendChild(createSubmit);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            // Travels to final page
+            window.location.replace("./HighScores.html");
+        }
+    });
+
 }
-
-var loadScores = function () {
-    var existingScoreEl = localStorage.getItem("dataArr");
-    existingScoreEl = JSON.parse(existingScoreEl);
-    for (var i = 0; i < existingScoreEl.length; i++) {
-        dataArr.push(existingScoreEl[i]);
-        scoreIdCounter++
-    }
-}
-var createScoreEl = function(){
-    var listItemEl =document.createElement("li");
-    listItemEl.classname = "score-item";
-    listItemEl.setAttribute("data-score-id", scoreIdCounter);
-    
-}
-
-
-start.onclick = startQuiz; 
-
-saveScore.onclick = enterScore; 
-
-loadScores();
